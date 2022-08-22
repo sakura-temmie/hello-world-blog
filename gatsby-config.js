0f3c5947f27bde9c4c8760e9f3b32540a98c5143
sakura miyamoto <sakura.m.208@gmail.com>
2022-08-22 11:40:15 +0900

    CMS 連携

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,28 @@
 /**
  * Configure your Gatsby site with this file.
  *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "Dev Blog",
+    description: "Gatsbyで作成したブログサイトです。",
+    author: "Engineer X",
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_API_KEY,
+      },
+    },
+  ],
 }
